b222dd1a89b4efccbdfb82ed821dc14b33371519
Alex Gilbert <118496831+gilbert-alex@users.noreply.github.com>
2024-06-26 23:59:51 -0700

    Function v1: Add button press handler
    Introduced logic based on classes in event.target class list to
    determine the if then logic. May need to refactor as the
    calculator main operation function is called.

--- script.js
+++ script.js
@@ -1,26 +1,11 @@
-// calculator helper functions
-const add = function (a, b) {
-    return a + b;
-}
-
-const subtract = function (a, b) {
-    return a - b;
-}
-
-const multiply = function (a, b) {
-    return a * b;
-}
-
-const divide = function (a, b) {
-    return a / b;
-}
 
 // variables
 let a = 0;
 let b = 0;
 let operator = '';
 
-// calculator operation
+
+// calculator main operation
 const operate = function (operator, a, b) {
     result = 0;
     switch (operator) {
@@ -41,3 +26,37 @@ const operate = function (operator, a, b) {
     }
     return result;
 }
+
+
+// calculator helper functions
+const add = function (a, b) {
+    return a + b;
+}
+
+const subtract = function (a, b) {
+    return a - b;
+}
+
+const multiply = function (a, b) {
+    return a * b;
+}
+
+const divide = function (a, b) {
+    return a / b;
+}
+
+
+let displayValue = '';
+let operation = '';
+
+const handleBtnPress = document.querySelector('.buttons');
+handleBtnPress.addEventListener('click', event => {
+    if (event.target.classList.contains('number')) {
+        displayValue += event.target.textContent;
+    } else if (event.target.classList.contains('operator')) {
+        console.log(displayValue);
+        displayValue = '';
+        operation = event.target.textContent;
+    }
+    
+})
